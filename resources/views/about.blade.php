fd826c4a346c8afd38e9595311e3aed4ba460ae7
Danson <christophedanwe@yahoo.com>
2020-11-11 00:05:45 +0100

    Add About page and Update links

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Hello City</title>
+        <title>About | Hello City</title>
 
         <!-- Fonts -->
 
@@ -14,9 +14,9 @@
     </head>
 
     <body>
-            <h1>Hello from Douala!</h1>
 
-            <p>It's currently {{ date('h:i A') }}</p>
+            <p>Built with &hearts; by DANWE Christophe</p>
+            <p><a href="/">Revenir à la page d'accueil</a></p>
 
             <footer>
                 <p>&copy; Copyright {{ date('Y') }} &middot; <a href="/about-us">About us</a></p>
