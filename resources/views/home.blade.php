25b7e7e2b10f1681b4b8e48d31c356913f953863
Danson <christophedanwe@yahoo.com>
2020-11-10 23:46:06 +0100

    Update Home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+
+        <!-- Styles -->
+
+
+    </head>
+
+    <body>
+            <h1>Hello from Douala!</h1>
+
+            <p>It's currently {{ date('h:i A') }}</p>
+
+            <footer>
+                <p>&copy; Copyright {{ date('Y') }} &middot; <a href="#">About us</a></p>
+            </footer>
+    </body>
+</html>
